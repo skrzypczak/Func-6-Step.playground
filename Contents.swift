6c06fd47a5efd062f495f59ae9394afa3fcde790
Bill Skrzypczak <bill@webbuildersconsulting.com>
2015-09-27 13:12:54 -0500

    Functions

---
+++ Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Func 6 Step Program"
+
+func doAnything(whatiLike: String) -> String {
+    let myfavorite = "I really like, " + whatiLike + "!"
+    return myfavorite
+}
+
+print(doAnything("Lager"))
+
+
+
+
+
